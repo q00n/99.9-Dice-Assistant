498ec0b20401208f065afcda79edf10d5c99cbfd
q00n <mrkeksikable@gmail.com>
2015-03-19 18:51:35 +0200

    Small fixes in injecting
    Removed unused files from "web_accessible_resources" list & injection
    files array

--- app/injector.js
+++ app/injector.js
@@ -3,9 +3,6 @@
     document.getElementsByTagName("body")[0].appendChild(script = document.createElement('script'));
     script.innerHTML = "var extension_id = '" + chrome.runtime.id + "';"
 
-    scripts.forEach(function (item)
-    {
-        document.getElementsByTagName("body")[0].appendChild(script = document.createElement('script'));
-        script.setAttribute('src', chrome.extension.getURL(item+".js"));
-    });
-}(["/app/config","/app/script"]));
+    document.getElementsByTagName("body")[0].appendChild(script = document.createElement('script'));
+    script.setAttribute('src', chrome.extension.getURL("/app/script.js"));
+}());
